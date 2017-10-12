a627447be730847b8e6d437706fdf8998b153675
PhillypHenning <phil8t8@gmail.com>
2017-10-12 08:34:36 -0400

    Robust entry point, class FileHandler

--- file-extractor.py
+++ file-extractor.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python
 
 # file_extractor
+import sys
+
+class FileHandler():
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
 
-
-def main():
-    print('hello world')
+def main(args):
+    new_dict = dict()
+    for arg in args:
+        key = arg.split('=')[0]
+        value = arg.split('=')[1]
+        new_dict[key] = value
 
 
 def __name__ == '__main__' :
-    main()
+    main(sys.argv[1:])
