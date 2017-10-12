976e5df0ac46a529d12c2c83a00173cd40cf5c4d
PhillypHenning <phil8t8@gmail.com>
2017-10-12 12:20:46 -0400

    Filename parser done

--- file-extractor.py
+++ file-extractor.py
@@ -2,18 +2,30 @@
 
 # file_extractor
 import sys
+import os.path
 
 class FileHandler():
     def __init__(self, **kwargs):
         self.file = kwargs.get('file')
 
+
     def verify(self):
-        print('TODO')
+        if not os.path.isfile(self.file) :
+            print('File not found, run file-extractor.py help for usage')
+            exit()
         
 
-
     def parse_file_name(self):
-        print(len(self.file)) 
+        out_file = ""
+        for c in reversed(self.file) :
+            print(c)
+            if c == '/' :
+                break
+            
+            else :
+                out_file += c
+
+        self.filename = out_file[::-1] 
 
 
     def byte_counter(self):
@@ -25,15 +37,17 @@ def main(args):
     Need        Command            Usage
     Start       run                file-extractor.py <PathtoFile> run
     """
+    
     if (len(args) > 2):
         print('Incorrect amount of arguements, run file-extractor.py help for usage')
         exit()
+    
     if(args[1] == 'run'):
         fh = FileHandler(
-            # Add keyword arg here for **kwargs 
-        )
+            **{'file' : args[0], })
 
-
+        fh.verify()
+        fh.parse_file_name()
 
     else:
         print(
@@ -43,9 +57,6 @@ def main(args):
             """
         )
         exit()
-
-
-
 
 
 if __name__ == '__main__' :
